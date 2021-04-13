2580af9de25cbb6129f492fe8ddbd31a8fb0e4e1
amirgamil <amir.amgad01@gmail.com>
2021-04-13 18:04:31 -0400

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Built with love by <a href = "http://amirbolous.com">Amir</a></p>
+        </footer>   
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <Footer />
+  )
+}
