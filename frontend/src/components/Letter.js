d4d2e3534a951b10f36e5aa33244774d5a5ad032
amirgamil <amir.amgad01@gmail.com>
2021-04-27 00:06:30 -0400

    fix title

--- frontend/src/components/Letter.js
+++ frontend/src/components/Letter.js
@@ -4,7 +4,7 @@ import Card from './Card'
 const Letter = ({template, title, content}) => {
     return (
         <div className={`${styles.letterCard} ${template ? styles.template : ""}`}>
-            <h1>{title}</h1>
+            <h1 style={{fontSize: "2.5em"}}>{title}</h1>
             {content.map((data, index) => {
                 return (
                     <Card key={"card" + index} data={data} />
